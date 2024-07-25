d80746ef2b8985cff36489c3f304850438f141ce
theub <theub@OMATHEUSBRITTO>
2024-07-25 00:09:58 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDeRepetcaoFOR/Program.cs
@@ -0,0 +1,23 @@
+﻿// Programa que trabalha a estrutura de repetição FOR
+using System;
+using System.Globalization;
+
+namespace EstruturaDeRepetcaoFOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            Console.Write("Quantos valores Deseja Digitar: ");
+            int n1 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n1; i++)
+            {
+                Console.Write("Digite o valor #{0}: ", i);
+                int n2 = int.Parse(Console.ReadLine());
+                soma += n2;
+            }
+            Console.WriteLine("Soma = {0}",soma);
+        }
+    }
+}
